f079882aa72400b9c85258948d326ce875a245e5
Kurt Axl Saludo <ewanmoakvevo@gmail.com>
2024-04-30 12:54:25 +0800

    updated villas

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: OnePage
-* Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
